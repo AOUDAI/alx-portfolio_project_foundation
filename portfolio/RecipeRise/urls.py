088a522ffba9ddd5d868694aa2c3cdc4e66f708b
AOUDAI <aoudair16031999@gmail.com>
2024-05-03 00:20:53 +0100

    second milestone

--- portfolio/RecipeRise/urls.py
+++ portfolio/RecipeRise/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.log, name="log"),
+    path("", views.log, name='log'),
     path('sign_up/', views.sign_up, name='sign_up'),
     path('sign_in/', views.sign_in, name='sign_in'),
     path('home/', views.home, name='home'),
